df17485809c770eed3125e0539ba663456bc9065
Arthur <121949966+ShinyWasabi@users.noreply.github.com>
2025-12-20 18:39:48 +0300

    feat: update natives and build numbers

--- src/app/Common.hpp
+++ src/app/Common.hpp
@@ -27,5 +27,5 @@ namespace scrDbgApp
     extern bool g_IsEnhanced;
 }
 
-#define ENHANCED_TARGET_BUILD "1013.17-1.72"
-#define LEGACY_TARGET_BUILD "3717.0-1.72"
+#define ENHANCED_TARGET_BUILD "1013.20-1.72"
+#define LEGACY_TARGET_BUILD "3725.0-1.72"
